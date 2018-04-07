ecbdf8b5f3852783a83f0cb7e60fb03dfcb287d7
Dominique Müller <dominique.m.mueller@gmail.com>
2018-04-08 00:44:10 +0200

    Fix content script execution within GitHub, switch to chrome typings, remove polyfill

--- src/background/index.ts
+++ src/background/index.ts
@@ -1,9 +1,16 @@
 import { TravisCiClient } from './travis-ci/travis-ci.client';
 import { TravisCiStages } from './travis-ci/travis-ci.interfaces';
 
-// Background
-
 console.log( 'RUNNING BACKGROUND!' );
+
+// Listen to navigation changes within GitHub itself
+chrome.webNavigation.onHistoryStateUpdated.addListener( ( details: chrome.webNavigation.WebNavigationTransitionCallbackDetails ): void => {
+    if ( /github\.com\/.*\/pull\/.*/.test( details.url ) ) {
+        chrome.tabs.executeScript( null, {
+            file: 'src/content_script.js'
+        } );
+    }
+} );
 
 const travisCiClient: TravisCiClient = new TravisCiClient();
 travisCiClient
--- src/content_script/index.ts
+++ src/content_script/index.ts
@@ -1,5 +1,3 @@
 // Content Script
 
 console.log( 'CONTENT SCRIPT RUNNING' );
-
-console.log( browser );
