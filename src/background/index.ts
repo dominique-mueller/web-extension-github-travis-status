2f7bc5434f9a352718940cd9a9d59974c12216ac
Dominique Müller <dominique.m.mueller@gmail.com>
2018-04-08 16:13:59 +0200

    Change implementation

--- src/background/index.ts
+++ src/background/index.ts
@@ -3,14 +3,26 @@ import { TravisCiStages } from './travis-ci/travis-ci.interfaces';
 
 console.log( 'RUNNING BACKGROUND!' );
 
-// Listen to navigation changes within GitHub itself
-chrome.webNavigation.onHistoryStateUpdated.addListener( ( details: chrome.webNavigation.WebNavigationTransitionCallbackDetails ): void => {
-    if ( /github\.com\/.*\/pull\/.*/.test( details.url ) ) {
-        chrome.tabs.executeScript( null, {
-            file: 'src/content_script.js'
-        } );
+// Setup connection to content script
+let contentScriptConnection: chrome.runtime.Port;
+chrome.runtime.onConnect.addListener( ( port: chrome.runtime.Port ): void => {
+    if ( port.name = 'content_script' ) {
+        contentScriptConnection = port;
     }
 } );
+
+// Listen to navigation changes within GitHub itself, send information to content script
+chrome.webNavigation.onHistoryStateUpdated.addListener(
+    ( details: chrome.webNavigation.WebNavigationTransitionCallbackDetails ): void => {
+        if ( /^https:\/\/github\.com\/.+\/.+\/pull\/\d+(\/commits)?$/.test( details.url ) ) {
+            if ( contentScriptConnection ) {
+                contentScriptConnection.postMessage( {
+                    type: 'navigation'
+                } );
+            }
+        }
+    }
+);
 
 const travisCiClient: TravisCiClient = new TravisCiClient();
 travisCiClient
--- src/content_script/index.ts
+++ src/content_script/index.ts
@@ -1,3 +1,12 @@
 // Content Script
 
 console.log( 'CONTENT SCRIPT RUNNING' );
+
+// document.body.appendChild( document.createComment( 'CONTENT SRIPT MANIPULATION' ) );
+
+const connectionPort: chrome.runtime.Port = chrome.runtime.connect( {
+    name: 'content_script'
+} );
+connectionPort.onMessage.addListener( ( message: any ) => {
+    console.log( message );
+} );
